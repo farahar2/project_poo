a84da57c71b449533db2f1429a3127d123faa85a
farahar2 <farahafouzar@gmail.com>
2026-03-27 17:04:02 +0100

    commit 5

---
+++ public/includes/head.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Icons -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- CSS personnalisé -->
+<link href="css/style.css" rel="stylesheet">
